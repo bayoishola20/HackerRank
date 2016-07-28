4a4d714c5e761d28b594e2f76fd87ca39ac315cf
Adebayo Yusuf Ishola <bayoishola20@yahoo.com>
2016-07-28 16:39:28 +0100

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,7 +1,14 @@
-Python 2.7.9 (default, Apr  2 2015, 15:33:21) 
-[GCC 4.9.2] on linux2
-Type "copyright", "credits" or "license()" for more information.
->>> #!/bin/python
+# LOOPS
+
+# Given an integer, N, print its first 10 multiples. Each multiple N x i (where 1 <= i <= 10) should be printed on a new line in the form: N x i = result.
+
+# Sample Input
+
+# 2
+
+# Sample Output
+
+# 2 x 1 = 2, 2 x 2 = 4, 2 x 3 = 6, 2 x 4 = 8, 2 x 5 = 10, 2 x 6 = 12, 2 x 7 = 14, 2 x 8 = 16, 2 x 9 = 18, 2 x 10 =20
 
 import sys
 
