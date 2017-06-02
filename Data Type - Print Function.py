ec93a1e516544cc664ffc9c2da6a424c2462cfa4
bayoishola20 <bayoishola20@yahoo.com>
2017-06-02 04:12:30 +0100

    Data-Type: Print function

---
+++ Data Type - Print Function.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import print_function
+N = int(raw_input())
+for i in range(1, N+1):
+    print (i, sep='', end='')
