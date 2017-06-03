4d2a25b5df498cd901b6be88bd5f49bb10549cc6
bayoishola20 <bayoishola20@yahoo.com>
2017-06-03 15:23:38 +0100

    Day 1 : More Exceptions

---
+++ Day 17 - More Exceptions.py
@@ -0,0 +1,20 @@
+#Write your code here
+class Calculator():
+    def power(self, n, p):
+        self.n = n
+        self.p = p
+        if n < 0 or p < 0:
+            return "n and p should be non-negative"
+        return pow(n, p)
+
+#==================== GIVEN ======================#
+myCalculator=Calculator()
+T=int(raw_input())
+for i in range(T):
+    n,p = map(int, raw_input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print ans
+    except Exception,e:
+        print e
+#===================== END =========================#
