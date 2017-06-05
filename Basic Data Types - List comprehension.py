88682cebd52311ac09f960631b96a99935adbbe7
bayoishola20 <bayoishola20@yahoo.com>
2017-06-05 15:53:37 +0100

    Basic Data Types - List comprehension

--- Basic Data Types - List comprehension.py
+++ Basic Data Types - List comprehension.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     y = int(raw_input())
     z = int(raw_input())
     n = int(raw_input())
-s#===================== END =========================#
+#===================== END =========================#
     
     ans = []
     [ans.append([x,y,z]) for x in xrange(0, x+1) for y in xrange(0, y+1) for z in xrange(0, z+1) if x + y + z !=n]
