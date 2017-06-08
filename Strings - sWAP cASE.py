eedb540b3794db9268f5d34dfee4477dcf2111e3
bayoishola20 <bayoishola20@yahoo.com>
2017-06-08 05:35:42 +0100

    Strings - sWAP cASE

---
+++ Strings - sWAP cASE.py
@@ -0,0 +1,10 @@
+def swap_case(s):
+    a = ''.join(map(str.swapcase, s))
+    return a
+
+#==================== GIVEN ======================#
+if __name__ == '__main__':
+    s = raw_input()
+    result = swap_case(s)
+    print result
+#===================== END =========================#
