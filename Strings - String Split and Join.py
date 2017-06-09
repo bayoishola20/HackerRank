7d9ab1b0b43f4b28d2e3c38786364b3e90b04b74
bayoishola20 <bayoishola20@yahoo.com>
2017-06-09 05:34:14 +0100

    Strings : String Split and Join

---
+++ Strings - String Split and Join.py
@@ -0,0 +1,5 @@
+def split_and_join(line):
+    # write your code here
+    x = line.split(" ")
+    y = "-".join(x)
+    return y
--- Strings - sWAP cASE.py
+++ Strings - sWAP cASE.py
@@ -6,5 +6,5 @@ def swap_case(s):
 if __name__ == '__main__':
     s = raw_input()
     result = swap_case(s)
-    print result
+    print resulta
 #===================== END =========================#
