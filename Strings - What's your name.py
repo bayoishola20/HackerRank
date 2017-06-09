691b11f0a8d898877bf741fa62f3f7ed3cd52b4f
bayoishola20 <bayoishola20@yahoo.com>
2017-06-09 05:37:53 +0100

    Strings : What's your name

---
+++ Strings - What's your name.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+name = raw_input()
+surName = raw_input()
+my_string = "Hello %s %s! You just delved into python." %(name, surName)
+print(my_string)
