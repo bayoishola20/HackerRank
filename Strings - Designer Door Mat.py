c4f07874124a08bf3dc5ef542c326e2bf4f4a861
bayoishola20 <bayoishola20@yahoo.com>
2017-06-11 03:06:40 +0100

    Strings : Designer Door Mat

--- Strings - Designer Door Mat.py
+++ Strings - Designer Door Mat.py
@@ -1,4 +1,4 @@
-N, M = map(int,raw_input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
+N, M = map(int,raw_input().split())
 for i in xrange(1,N,2): 
     print ('.|.'*i).center(M, '-')
 print 'WELCOME'.center(M, '-')
