acb04acd7de599b0104166c8f052f0008880a819
bayoishola20 <bayoishola20@yahoo.com>
2017-10-03 23:41:22 +0100

    Sets: Set .union() Operation

---
+++ Sets - Set .union() Operation.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = (raw_input())
+n_set = set(raw_input().split()) #split: to split characters
+
+b = (raw_input())
+b_set = set(raw_input().split()) #split: to split characters
+
+print len(n_set.union(b_set))
