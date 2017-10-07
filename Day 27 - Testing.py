1d414d8ffdd1eb527f752b3d6f34f43d676aab97
bayoishola20 <bayoishola20@yahoo.com>
2017-10-07 07:19:22 +0100

    edit

--- Day 27 - Testing.py
+++ Day 27 - Testing.py
@@ -1,11 +1,11 @@
-print("5")
-print("4 3")
-print("0 -3 4 2")
-print("5 2")
-print("0 -3 4 2 2")
-print("3 3")
-print("0 -3 4")
-print("7 2")
-print("0 -3 1 1 1 1 1")
-print("6 3")
-print("0 -3 4 2 1 1")
+print("5") #t = 5
+print("4 3") #t k
+print("0 -3 4 2") #YES
+print("5 2") #t k
+print("0 -3 3 3 2") #NO
+print("3 3") #t k
+print("0 -3 4") #YES
+print("7 2") #t k
+print("0 -3 1 1 1 1 1") #NO
+print("6 3") #t k
+print("0 -3 2 2 2 2") #YES
