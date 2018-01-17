3a298e91d50872b588254f55f999da21a4f7675f
bayoishola20 <bayoishola20@yahoo.com>
2018-01-17 17:11:32 +0100

    Day 2: JavaScript

---
+++ 10 Days of Javascript/Day 2: Conditional Statements: If-Else.js
@@ -0,0 +1,23 @@
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    if (score > 25 && score <= 30 ) {
+        return grade = "A";
+    }
+    else if (score > 20 && score <= 25 ) {
+        return grade = "B";
+    }
+    else if (score > 15 && score <= 20 ) {
+        return grade = "C";
+    }
+    else if (score > 10 && score <= 15 ) {
+        return grade = "D";
+    }
+    else if (score > 5 && score <= 10 ) {
+        return grade = "E";
+    }
+    else if (score > 0 && score <= 5 ) {
+        return grade = "F";
+    }
+    return grade;
+}
