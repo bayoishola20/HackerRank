3d023eb176ec46f717b293665a2fcc42fa98af65
bayoishola20 <bayoishola20@yahoo.com>
2018-01-19 05:28:51 +0100

    Day 3: JavaScript

---
+++ 10 Days of Javascript/Day 3: Throw.js
@@ -0,0 +1,17 @@
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    try {
+        if (a > 0) throw "YES";
+        if (a === 0) throw ("Zero Error");
+        if (a < 0) throw ("Negative Error");
+    }
+    
+    catch (err) {
+        return (err)
+    }
+}
