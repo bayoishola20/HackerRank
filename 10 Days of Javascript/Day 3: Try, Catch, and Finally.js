b5e5799fa83f979ddb5b6ce348d75d7ae173ffe7
bayoishola20 <bayoishola20@yahoo.com>
2018-01-19 04:39:49 +0100

    Day 3: JavaScript

---
+++ 10 Days of Javascript/Day 3: Try, Catch, and Finally.js
@@ -0,0 +1,14 @@
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try {
+     console.log(s.split("").reverse().join(""));   
+    }
+    
+    catch (err) {
+        console.log(err.message);
+        console.log(s);
+    }  
+}
