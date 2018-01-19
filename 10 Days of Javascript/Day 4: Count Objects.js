29fa6fbd41989b85238f6122fcbe424e32005e50
bayoishola20 <bayoishola20@yahoo.com>
2018-01-19 06:55:44 +0100

    Day 4: JavaScript

---
+++ 10 Days of Javascript/Day 4: Count Objects.js
@@ -0,0 +1,12 @@
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {    
+    let obj = function(o){
+        return o.x == o.y
+    }
+    return objects.filter(obj).length;
+}
