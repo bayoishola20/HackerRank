64639ca19ba5b6b8036bd741d089a1cf645748c0
bayoishola20 <bayoishola20@yahoo.com>
2018-01-21 04:45:34 +0100

    Day 6: Javascript

---
+++ 10 Days of Javascript/Day 6: Bitwise Operators.js
@@ -0,0 +1,56 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// ===============================
+    // Code in stub above //
+// ===============================
+
+/*
+*	Return the largest value of any a & b < k in S (where a < b).
+* 	
+*	n: Set S is a sequence of distinct integers from 1 to n (i.e., {1, 2, ..., n}).
+*	k: An integer.
+*/
+function getMaxLessThanK(n, k) {
+    if ((k | (k - 1)) > n) {
+        return k - 2;
+    } else {
+        return k - 1;
+    }
+}
+// | - numeric operand in the operation is converted into a 32 bit number. The result is converted back to a JavaScript number.
+
+// ===============================
+    // Code in stub below //
+// ===============================
+
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
