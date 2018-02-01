9f0d8ff71a6434bcc67b9f122d44161b82705a06
bayoishola20 <bayoishola20@yahoo.com>
2018-02-01 14:46:41 +0100

    Day 6: Statistics

---
+++ 10 Days of Statistics/Day 6: The Central Limit Theorem II.py
@@ -0,0 +1,14 @@
+import math
+last_minute = int(raw_input())
+N = int(raw_input())
+mean = float(raw_input())
+std = float(raw_input())
+
+def cum_dist(mean, std, x):
+    return (0.5) * (1 + math.erf((x - mean) / (std * (math.sqrt(2)))))
+
+mean *= N #new mean
+std *= math.sqrt(N) #new std
+
+
+print round(cum_dist(mean, std, last_minute), 4)
